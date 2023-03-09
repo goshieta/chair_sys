26f474a1d17e497da20477ce3ef63234cb1921f4
goshieta <117492010+goshieta@users.noreply.github.com>
2023-03-09 16:21:17 +0900

    ページとヘッダーを大まかに作成

---
+++ layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="header">
+    <h1 id="appTitle">席替え革命</h1>
+    <div id="linkArea">
+      <a href="">作成</a>
+      <a href="">アップロード</a>
+      <a href="">このサイトについて</a>
+      <a href="https://iyqzbugtwfgzyugaybuxxa.on.drv.tw/Indorde%20top/InT.html"
+        >Inforde</a
+      >
+    </div>
+  </div>
+  <slot />
+</template>
+
+<style>
+* {
+  font-family: '游ゴシック体', 'YuGothic', '游ゴシック', 'Yu Gothic',
+    'ヒラギノ角ゴ Pro W3', 'Hiragino Kaku Gothic Pro', 'メイリオ', 'Meiryo',
+    sans-serif;
+}
+#appTitle {
+  font-size: 25px;
+}
+#header {
+  display: flex;
+  align-items: center;
+}
+#linkArea {
+  display: flex;
+  gap: 20px;
+  margin-right: 50px;
+  margin-left: auto;
+}
+#linkArea a {
+  color: black;
+  text-decoration: none;
+}
+</style>
