5299e13cda9da6425cd34e7bb063ab1aa37af6e5
goshieta <117492010+goshieta@users.noreply.github.com>
2023-03-10 11:37:29 +0900

    metaタグなどを設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ['/assets/css/style.css'],
+  app: {
+    head: {
+      title: '席替え革命',
+      meta: [
+        {
+          name: 'description',
+          content:
+            'くじ引きでの席替えはもうやめませんか？席替え革命を使うと簡単にリッチな画面で席替えをすることが出来ます。',
+        },
+      ],
+      link: [
+        {
+          rel: 'icon',
+          href: '/favicon.png',
+        },
+      ],
+    },
+  },
 });
